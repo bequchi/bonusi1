e7fb4a603431a51e496f2e2b1b2d361f145c6389
beka <beka.tchankvetadze.1@btu.edu.ge>
2021-01-29 15:18:47 +0400

    first commit

---
+++ app/src/main/java/com/example/bonusi/DataFragment.kt
@@ -0,0 +1,67 @@
+package com.example.bonusi
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.iamzeus.databinding.FragmentDataBinding
+
+class DataFragment() : Fragment(), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FragmentDataBinding.bind(view).onViewBind()
+    }
+
+    private fun FragmentDataBinding.onViewBind() {
+        val sharedPref = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
+
+        val firstNamePref = sharedPref?.getString("user_name", "")
+        val lastNamePref = sharedPref?.getString("user_last_name", "")
+        val agePref = sharedPref?.getString("user_age", "")
+
+        if (firstNamePref?.isEmpty() == true || lastNamePref?.isEmpty() == true || agePref?.isEmpty() == true) {
+            Toast.makeText(context, "Information is not entered fully.", Toast.LENGTH_SHORT)
+                    .show()
+        }
+
+        firstName.text = "Name: $firstNamePref"
+        lastName.text = "Last name: $lastNamePref"
+        age.text = "Age: $agePref"
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DataFragment> {
+        override fun createFromParcel(parcel: Parcel): DataFragment {
+            return DataFragment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DataFragment?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
